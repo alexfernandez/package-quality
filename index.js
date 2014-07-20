acd57dc8b41f3232495f819ad5afd135e1a4a3ac
Alex Fernández <alex.fernandez@mediasmart.es>
2014-07-20 18:52:33 +0200

    Index and test.

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * Export everything.
+ * (C) 2014 Alex Fernández.
+ */
+
+
+// requires
+var estimation = require('./lib/estimation.js');
+
+// exports
+exports.estimate = estimation.estimate;
+
+
---
+++ test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * Run package tests.
+ * (C) 2014 Alex Fernández.
+ */
+
+// requires
+var testing = require('testing');
+var Log = require('log');
+
+// globals
+var log = new Log('info');
+
+
+/**
+ * Run all module tests.
+ */
+exports.test = function(callback)
+{
+	log.debug('Running tests');
+	var tests = {};
+	var libs = ['estimate'];
+	libs.forEach(function(lib)
+	{
+		tests[lib] = require('./lib/' + lib + '.js').test;
+	});
+	testing.run(tests, 4200, callback);
+};
+
+// run tests if invoked directly
+if (__filename == process.argv[1])
+{
+	exports.test(testing.show);
+}
+
