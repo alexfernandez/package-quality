235eb79858ad5b87545f8db57816578d66117aab
Sergio García <sgmonda@gmail.com>
2015-02-10 18:48:40 +0100

    Basic material client app

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+var app = angular.module('StarterApp', ['ngMaterial']);
+
+app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+	$scope.toggleSidenav = function(menuId) {
+		$mdSidenav(menuId).toggle();
+	};
+}]);
