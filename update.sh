f5469bdc40ab15fe9309928a3aa1600e339f79ea
DiegoTUI <dlafuente@tuitravel-ad.com>
2015-02-10 09:08:14 +0100

    update script

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Update packages
+HOME=/home/ubuntu
+USER=ubuntu
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+cd $DIR/bin
+
+sudo -u $USER nodejs update.js >> $HOME/log/package-quality-update.log
