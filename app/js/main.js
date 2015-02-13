79bf9b1c0aac0d286bd4d30e783ee26f0f5ad6f1
sgmonda <sgmonda@gmail.com>
2015-02-13 10:16:47 +0100

    Basic search working

--- app/js/main.js
+++ app/js/main.js
@@ -2,42 +2,102 @@
 
 var app = angular.module('PackageQuality', ['ui.bootstrap']);
 
-app.controller('MainController', ['$scope', '$timeout', function($scope, $timeout){
+app.factory('packages', ['$http', function ($http) {
 	'use strict';
 
-	$scope.packages = ['sergio', 'pepe', 'serio', 'josete', 'pepita'];
+	return {
+		get: function (packageName, callback) {
+			var url = '/package/' + packageName;
+			$http.get(url).success(function (result) {
+				callback(null, result);
+			}).error(function (description, status) {
+				callback({error: description, status: status});
+			});
+		}
+	};
+}]);
+
+app.filter('timeAgo', function () {
+	'use strict';
+
+	return function (date, attributesArray) {
+
+		var attributes = {};
+		attributesArray.forEach(function (attr) {
+			attributes[attr] = true;
+		});
+
+		if (typeof date === 'string') {
+			date = new Date(Date.parse(date));
+		}
+		var date2 = new Date();
+		var diff = date2 - date;
+
+		var msec = diff;
+		var YY = Math.floor(msec / 1000 / 60 / 60 / 24 / 365);
+		msec -= YY * 1000 * 60 * 60 * 24 * 365;
+		var MM = Math.floor(msec / 1000 / 60 / 60 / 24 / 30);
+		msec -= MM * 1000 * 60 * 60 * 24 * 30;
+		var DD = Math.floor(msec / 1000 / 60 / 60 / 24);
+		msec -= DD * 1000 * 60 * 60 * 24;
+		var hh = Math.floor(msec / 1000 / 60 / 60);
+		msec -= hh * 1000 * 60 * 60;
+		var mm = Math.floor(msec / 1000 / 60);
+		msec -= mm * 1000 * 60;
+		var ss = Math.floor(msec / 1000);
+		msec -= ss * 1000;
+
+		return [
+			((YY && (!attributes || attributes.years)) ? YY + ' years ': ''),
+			((MM && (!attributes || attributes.months)) ? MM + ' months ': ''),
+			((DD && (!attributes || attributes.days)) ? DD + ' days ': ''),
+			((hh && (!attributes || attributes.hours)) ? hh + ' hours ': ''),
+			((mm && (!attributes || attributes.minutes)) ? mm + ' minutes ' : ''),
+			((ss && (!attributes || attributes.seconds)) ? ss + ' seconds ' : '')
+		].join('') + ' ago';
+	};
+});
+
+app.controller('MainController', ['$scope', 'packages', function($scope, packages){
+	'use strict';
+
+	$scope.packages = []; // Add items to autocomplete
 	$scope.query = null;
 	$scope.package = null;
-	$scope.metrics = ['repoQuality', 'downloadsQuality', 'versionsQuality'];
+	$scope.metrics = [{
+		key: 'quality',
+		label: 'Package quality'
+	}, {
+		key: 'repoQuality',
+		label: 'Repo quality'
+	}, {
+		key: 'downloadsQuality',
+		label: 'Downloads quality'
+	}, {
+		key: 'versionsQuality',
+		label: 'Versions quality'
+	}];
 
-	$scope.$watch('query', function (query) {
-		if ($scope.packages.indexOf(query) === -1) {
+	$scope.genArray = function (count) {
+		return new Array(count);
+	};
+
+	$scope.getStars = function (pack) {
+		return Math.floor(pack.quality * 5);
+	};
+
+	$scope.keyup = function (isEnter) {
+		if (!isEnter) {
 			return;
 		}
-		console.log('query chages to "%s"', query);
-
 		$scope.isLoadingPackage = true;
-		$timeout(function () {
+		packages.get($scope.query, function (err, result) {
 			$scope.isLoadingPackage = false;
-			$scope.package = {
-				"_id": "54cb2b46465dcde6761dc044",
-				"name": "stdio",
-				"source": "npm",
-				"repoQuality": 0.9761904761904763,
-				"repoFactors": {
-					"totalFactor": 0.9285714285714286,
-					"openFactor": 1,
-					"longOpenFactor": 1
-				},
-				"downloadsQuality": 0.9999891826403011,
-				"versionsQuality": 0.9444444444444444,
-				"quality": 0.9219476988099072,
-				"created": "2015-02-06T11:06:21+00:00",
-				"lastUpdated": "2015-02-11T01:56:16+00:00",
-				"nextUpdate": "2015-02-11T01:56:16+00:00",
-				"timesUpdated": 2
-			};
-		}, 1000);
-
-	});
+			if (err) {
+				$scope.package = {query: $scope.query, notfound: true};
+				return;
+			}
+			$scope.package = result;
+		});
+	};
 }]);
