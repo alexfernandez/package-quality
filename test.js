6b8f4e9db1488d3ba98d1edaaad5a49b3d1b0562
DiegoTUI <dlafuente@tuitravel-ad.com>
2015-02-17 10:36:20 +0100

    finished unit tests in downloads

--- test.js
+++ test.js
@@ -21,7 +21,7 @@ exports.test = function(callback)
 	log.debug('Running tests');
 	var tests = {};
 	var libs = ['estimation', 'db'];
-    var factors = ['issues', 'versions'];
+    var factors = ['issues', 'versions', 'downloads'];
     var bin = ['all', 'update'];
 	libs.forEach(function(lib)
 	{
